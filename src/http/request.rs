b8ffd7e39eaa6fe4a30440a2a7e86d053d29a30c
galser <guseletov@gmail.com>
2025-12-03 14:20:56 +0100

    Custom errors

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,5 +1,7 @@
 use super::method::Method;
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult}; 
 
 /// Request struc
 ///
@@ -9,18 +11,62 @@ pub struct Request {
     method: super::method::Method,
 }
 
-impl Request {
-    // fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
-        
-    // }
-    
-}
-
-
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
+/// Raw request example 
+/// GET /test HTTP/1.1
+/// Host: 127.0.0.1:8080
+/// Connection: keep-alive
+/// Cache-Control: max-age=0
+/// sec-ch-ua: "Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"
+/// sec-ch-ua-mobile: ?0
+/// sec-ch-ua-platform: "macOS"
+/// Upgrade-Insecure-Requests: 1
+/// User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36
+/// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+/// Sec-Fetch-Site: none
+/// Sec-Fetch-Mode: navigate
+/// Sec-Fetch-User: ?1
+/// Sec-Fetch-Dest: document
+/// Accept-Encoding: gzip, deflate, br, zstd
+/// Accept-Language: en-US,en;q=0.9,ru;q=0.8,uk;q=0.7,nl;q=0.6
+
+    type Error = ParseError;
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         unimplemented!()
     }    
 }
+
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol, // not HTTP 1.1 
+    InvalidMethod,
+}
+
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}!", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}!", self.message())
+    }
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Inbalid Method",
+        }
+    }
+}
+
+impl Error for ParseError {}
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 use std::io::Read;
+//use super::Request;
+use crate::http::Request;
+use std::convert::TryFrom;
 
 pub struct Server {
     addr: String,
@@ -40,7 +43,12 @@ impl Server {
                     let mut buffer = [0; 1024];
                     match stream.read(&mut buffer) { 
                      Ok(bytes) => {
-                        println!("Received a request {}", String::from_utf8_lossy(&buffer))
+                        println!("Received a request {}", String::from_utf8_lossy(&buffer));
+//                        Request::try_from(&buffer as &[u8]);
+                        match Request::try_from(&buffer[..]) {
+                            Ok(request) => {},
+                            Err(e) => println!("Failed to parse a request {}", e),
+                        }
                      },
                      Err(e) => println!("failed to read from connection {}", e),
 
